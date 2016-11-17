c87a6edc1894ea39ef3ef232978638c632bc3d7b
Maurício de Lima <jmauriciolm38@gmail.com>
2016-11-17 17:10:02 -0300

    first code

---
+++ algoritmo/algoritmo.ino
@@ -0,0 +1,13 @@
+int IN1 = 4;
+int IN2 = 5;
+int IN3 = 6;
+int IN4 = 7;
+  
+void setup()
+{
+  //Define os pinos como saida
+ pinMode(IN1, OUTPUT);
+ pinMode(IN2, OUTPUT);
+ pinMode(IN3, OUTPUT);
+ pinMode(IN4, OUTPUT);
+}

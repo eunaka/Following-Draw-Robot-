1bab3abddb733f6d7434d4cf40a43483e5731077
Maurício de Lima <jmauriciolm38@gmail.com>
2016-11-18 21:37:08 -0300

    Comands with keyboard

--- algoritmo/algoritmo.ino
+++ algoritmo/algoritmo.ino
@@ -2,12 +2,94 @@ int IN1 = 4;
 int IN2 = 5;
 int IN3 = 6;
 int IN4 = 7;
+char ligar = 0;
+int controle=0;
   
 void setup()
 {
+  Serial.begin(9600);
   //Define os pinos como saida
  pinMode(IN1, OUTPUT);
  pinMode(IN2, OUTPUT);
  pinMode(IN3, OUTPUT);
  pinMode(IN4, OUTPUT);
+}
+
+void loop(){
+  if(Serial.available()){
+    ligar = Serial.read();
+    Serial.println(ligar);
+    if(ligar == 'w'){
+      controle = 1;
+      acelerar();
+    }
+    else if(ligar == 'a'){
+      controle = 1;
+      virar_esquerda();
+    }
+    else if(ligar == 'd'){
+      controle = 1;
+      virar_direita();
+    }
+    else if(ligar == 's'){
+      controle = 1;
+      desacelerar();
+    }
+    else {
+    controle =1;
+    parar();
+    }
+  }
+  if(controle == 1){
+   // delay(2000);
+    controle = 0;
+  }
+  
+ //s parar();
+  //acelerar();
+}
+
+void acelerar(){
+  //RODA ESQUERDA
+  digitalWrite(IN1, LOW); 
+  digitalWrite(IN2, HIGH);
+  // RODA DIREITA
+  digitalWrite(IN3, HIGH); 
+  digitalWrite(IN4, LOW);  
+}
+
+void desacelerar(){
+  //RODA ESQUERDA
+  digitalWrite(IN1, HIGH); 
+  digitalWrite(IN2, LOW);
+  // RODA DIREITA
+  digitalWrite(IN3, LOW); 
+  digitalWrite(IN4, HIGH);  
+}
+
+void virar_esquerda(){
+  //RODA ESQUERDA
+  digitalWrite(IN1, LOW); 
+  digitalWrite(IN2, LOW);
+  // RODA DIREITA
+  digitalWrite(IN3, HIGH); 
+  digitalWrite(IN4, LOW);  
+}
+
+void virar_direita(){
+ //RODA ESQUERDA
+  digitalWrite(IN1, LOW); 
+  digitalWrite(IN2, HIGH);
+  // RODA DIREITA
+  digitalWrite(IN3, LOW); 
+  digitalWrite(IN4, LOW);   
+}
+
+void parar(){
+  //RODA ESQUERDA
+  digitalWrite(IN1, LOW); 
+  digitalWrite(IN2, LOW);
+  // RODA DIREITA
+  digitalWrite(IN3, LOW); 
+  digitalWrite(IN4, LOW);  
 }
